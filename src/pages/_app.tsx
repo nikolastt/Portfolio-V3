704466b4315f8a367afee13c54e652410bd480b6
Nikolas <nikolasbitencourtt@gmail.com>
2023-03-30 04:56:30 -0300

    Add og metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,28 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Nikolas Bitencourt</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Nikolas Bitencourt" />
+        <meta
+          property="og:description"
+          content="Desenvolvedor React JS | Front-end | Java Script | Typescript | Next JS |"
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/portfolio-9c731.appspot.com/o/Logo.png?alt=media&token=2fd11c6e-afba-4a37-8c13-e6abc3e47fa3"
+        />
+        <meta property="og:image:wigth" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta
+          property="og:url"
+          content="https://nikolasbitencourt.vercel.app"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Nikolasbite" />
         <meta
           name="description"
-          content="Desenvolvedor Front-end | React JS | Java Script | Typescript | Next JS |"
+          content="Desenvolvedor Front-end | React JS | Java Script | Typescript | Next JS"
         />
         <meta name="author" content="Nikolas Bitencourt" />
         <meta
@@ -20,6 +38,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="google-site-verification"
           content="C4ne6DifY4pvBeJI0jriXYaLdVoafoAncatYdvK0dB8"
         />
+        <title>Nikolas Bitencourt</title>
         <link rel="icon" href="/images/NB.svg" />
       </Head>
 
